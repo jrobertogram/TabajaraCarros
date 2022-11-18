3c0b36c95e91f912eb44732144c4c8329738d605
Roberto <33937381+jrobertogram@users.noreply.github.com>
2022-11-18 20:40:39 -0300

    att

---
+++ Carro.java
@@ -0,0 +1,10 @@
+public abstract class Carro {
+    private String ID;
+    private String Brand;
+    private String Model;
+    private int Year;
+    private String Price;
+    private String Type;
+    private String Department;
+
+}
