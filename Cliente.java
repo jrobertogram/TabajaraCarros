dbddce14151fddb835ad2a470862ca2402008a39
jr22ar <65619631+jr22ar@users.noreply.github.com>
2022-11-18 20:59:36 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -13,4 +13,35 @@ public abstract class Cliente {
       Type = type;
     }
   
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getDocument() {
+        return Document;
+    }
+    public void setDocument(String document) {
+        Document = document;
+    }
+    public String getCity() {
+        return City;
+    }
+    public void setCity(String city) {
+        City = city;
+    }
+    public String getType() {
+        return Type;
+    }
+    public void setType(String type) {
+        Type = type;
+    }
+  
 }
