d970bcceb86306c5f9d59f4587e134e7bcd9b049
Roberto <33937381+jrobertogram@users.noreply.github.com>
2022-11-18 20:42:54 -0300

    att

--- Carro.java
+++ Carro.java
@@ -7,4 +7,15 @@ public abstract class Carro {
     private String Type;
     private String Department;
 
+    public Carro(String brand, String model, int year, String price, String type, String department) {
+       
+        this.Brand = brand;
+        this.Model = model;
+        this.Year = year;
+        this.Price = price;
+        this.Type = type;
+        this.Department = department;
+        
+    }
+
 }
